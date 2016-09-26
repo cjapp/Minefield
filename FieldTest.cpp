9bd1133456c6e9ab9c6f8a1b5552ec4b9c8eaea3
cjapp <cjapp@mail.csuchico.edu>
2016-09-26 11:58:41 -0700

    Submitting, lack of time during period

--- FieldTest.cpp
+++ FieldTest.cpp
@@ -22,3 +22,19 @@ TEST(FieldTest, placeMineInBounds)
 	minefield.placeMine(4,5);
 	ASSERT_EQ( MINE_HIDDEN, minefield.get(4,5) );
 }
+
+TEST(FieldTest, placeMineInBounds_0x0)
+{
+	Field minefield;
+	
+	minefield.placeMine(0,0);
+	ASSERT_EQ( MINE_HIDDEN, minefield.get(0,0) );
+}
+
+TEST(FieldTest, inbounds_overload constructor)
+{
+		Field minefield(15)
+
+	Minefield.placeMine(12,12);
+
+}
--- buglog.md
+++ buglog.md
@@ -1,1 +1,3 @@
 # BUG LOG
+
+There isnt any code to update the FIELD_DIMENSION if the overload constructor is called with the new dimensions; this causes issues with the is_indounds function.
